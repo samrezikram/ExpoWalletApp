98da271c4fdc6669e32aec1c6329295372dd8fe9
samrez Ikram <samrezikram@yahoo.com>
2023-01-05 20:27:30 +0400

    - init setup - Welcome screen

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+import AppLoading from "expo-app-loading"; 
+import { useFonts } from "expo-font"
+
+import Welcome from './screens/Welcome';
+
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "Lato-Bold": require("./assets/fonts/Lato-Bold.ttf"),
+    "lato-Regular": require("./assets/fonts/Lato-Regular.ttf")
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Welcome />
   );
 }
 
---
+++ components/Texts/SmallText.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from "react" 
+import styled from "styled-components/native"
+
+//colors
+import { colors } from "../colors"
+const StyledText = styled.Text`
+  font-size: 13px;
+  color: ${colors.gray};
+  text-align: left;
+  font-family: Lato-Regular;
+`;
+const SmallText: FunctionComponent = () => { 
+  return <></>;
+};
+
+export default SmallText;
